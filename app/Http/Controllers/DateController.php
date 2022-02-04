428f2e54d947c6c4c821c25a58bce34a7775eefd
acmtps <acmtps@gmail.com>
2022-02-04 18:13:58 +1030

    Date Controller created

--- app/Http/Controllers/DateController.php
+++ app/Http/Controllers/DateController.php
@@ -13,6 +13,95 @@ use Illuminate\Validation\ValidationException;
 class DateController extends Controller
 {
 
-   
+   /**
+     * intialize Datehelper Class from servive provider binding
+     * @dependency Injection DI DateHelper class resolve DateHelper class
+     */
+    public function __construct(DateHelper $dateHelper)
+    {
+        $this->dateHelper = $dateHelper ;
+    }
+
+    /**
+     * @param Api\Request $request optional //optional request parameter  may coming
+     * @return  responseJson int|string
+     */
+    public function getDayDiffNumber(DateRequest $request)
+    {
+        try{
+            $diffNumber = $this->dateHelper->getDaysDiff($request->date1, $request->date2, $request->format);
+        }catch(Exception $e){
+
+            return response(['error' => $e->getMessage(), ], 200);
+        }
+        return response(['numberofday' => $diffNumber, 'format' => $request->format],200);
+
+    }
+
+    /**
+     * @param Api\Request $request optional //optional request parameter  may coming
+     * @param Request $request data types date format and strings
+     * @return  responseJson int|string
+     */
+    public function getWeekDiffNumber(DateRequest $request)
+    {
+        try{
+            $diffNumber = $this->dateHelper->getWeekDiff($request->date1, $request->date2, $request->format);
+        }catch(Exception $e){
+
+            return response(['error' => $e->getMessage(), ], 200);
+        }
+        return response(['numberofweeks' => $diffNumber, 'format' => $request->format],200);
+
+    }
+
+    /**
+     * @param Api\Request $request optional //optional request parameter  may coming
+     * @param Request $request data types date format and strings
+     * @return  responseJson int|string
+     */
+    public function getWeeksDays(DateRequest $request)
+    {
+        try{
+            $weekDays = $this->dateHelper->getWeeksDays($request->date1, $request->date2, $request->format);
+        }catch(Exception $e){
+
+            return response(['error' => $e->getMessage(), ], 200);
+        }
+        return response(['weekdays' => $weekDays, 'format' => $request->format],200);
+
+    }
+
+    /**
+     * @param Api\Request $request optional //optional request parameter  may coming
+     * @param Request $request data types date format and strings
+     * @return  responseJson int|string
+     */
+    public function conventIntoFormat(DateRequest $request)
+    {
+        try{
+            $diffNumber = $this->dateHelper->convertDate($request->date1, $request->date2, $request->format);
+        }catch(Exception $e){
+            return response(['error' => $e->getMessage(), ], 200);
+        }
+        return response(['formated_date' => $diffNumber, 'format' => $request->format],200);
+
+    }
+
+     /**
+     * @param Api\Request $request optional //optional request parameter  may coming
+     * @param Request $request data types date format and strings
+     * @return  responseJson int|string
+     */
+    public function compareTimeZone(DateRequest $request)
+    {
+        try{
+            $diffNumber = $this->dateHelper->timeZoneComparasion($request->date1, $request->date2, $request->format);
+        }catch(Exception $e){
+            return response(['error' => $e->getMessage(), ], 200);
+        }
+        return response(['formated_date' => $diffNumber, 'format' => $request->format],200);
+
+    }
 
 }
