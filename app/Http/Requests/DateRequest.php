2b13bb7997b536b4b71f7a6e886648280ee7ce4c
acmtps <acmtps@gmail.com>
2022-02-04 18:12:14 +1030

    Date request created

--- app/Http/Requests/DateRequest.php
+++ app/Http/Requests/DateRequest.php
@@ -7,5 +7,27 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class DateRequest extends FormRequest
 {
-    
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'date1' => 'required',
+            'date2' => 'required',
+            'format' => ['sometimes', new DateFormatRule()],
+        ];
+    }
 }
